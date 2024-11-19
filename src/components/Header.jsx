b864ef68ec340a926e924b9a9d7cc0511f6003bf
Jessica Moore <jessica.cmoore1@gmail.com>
2024-11-19 13:15:02 -0500

    :lipstick: Header styles and font change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,18 +12,17 @@ const Header = () => {
   }, []);
   return (
     <header role="banner" className="relative py-32">
-      <div className="container relative z-10 mx-auto">
-        <h1 className="text-7xl font-black sm:text-8xl">
-          Jessica Moore.{" "}
-          <span className="mt-2 block text-5xl sm:text-6xl">
-            Fullstack Developer.
+      <div className="container relative z-10 mx-auto text-center">
+        <h1 className="text-7xl font-black text-gray-800 sm:text-8xl">
+          Jessica Moore{" "}
+          <span className="mt-6 block text-5xl sm:text-6xl">
+            Fullstack Developer
           </span>
         </h1>
-        <p className="my-6">
-          Fullstack developer blending creativity with tech to make things work
-          beautifully.
+        <p className="my-8 text-xl font-medium sm:text-2xl">
+          I like to blend creativity with tech to make things work beautifully.
         </p>
-        <div className="cta-buttons flex flex-wrap gap-2">
+        <div className="cta-buttons flex flex-wrap justify-center gap-2">
           <Button href="https://linkedin.com/in/jesscodes" target="_blank">
             <SiLinkedin className="mb-1 mr-1 inline-block" /> Let's connect!
           </Button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     container: {
-      padding: '2rem',
+      padding: "2rem",
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
