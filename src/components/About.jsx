3d4e9c5315d4d3922170ef3389746cf9d59c8028
Jessica Moore <jessica.cmoore1@gmail.com>
2024-11-19 13:10:36 -0500

    :speech_balloon: Update about me content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,32 +6,21 @@ const About = () => {
     <Section id="about" className="relative z-10 -mt-12 bg-white pt-6">
       <SectionTitle>A little bit about me.</SectionTitle>
       <p>
-        My name is Jessica, but I'd love it if you called me Jess. I'm a
-        fullstack developer with a frontend focus and passion for crafting
-        accessible, user-centered experiences. Since transitioning from
-        marketing to web development in 2019, I’ve grown quickly—from landing a
-        web developer role to leading a team where I mentored developers and
-        championed best practices for efficient, high-quality, accessible
-        solutions. With a strong background in React, JavaScript, CSS/SASS, and
-        user-focused design, I’m committed to building interfaces that are
-        inclusive and seamless.
+        Hi, I’m Jess—a fullstack developer with a frontend focus, and I'm
+        passionate about creating accessible, user-centered experiences. Since
+        transitioning from marketing to web development in 2019, I’ve quickly
+        progressed from a web developer role to leading a team, mentoring
+        developers, and championing best practices. I specialize in React,
+        JavaScript, and CSS/SASS, building seamless, inclusive interfaces.
       </p>
       <p>
-        As I continue to refine my frontend expertise, I focus on expanding my
-        skill set with modern tools and frameworks that enhance the user
-        experience and streamline development. Adaptable and collaborative, I
-        thrive in roles that allow me to blend creativity with technical
-        precision, always with an eye on innovation and accessibility.
-      </p>
-      <p>
-        When I’m not coding, you’ll find me exploring creative hobbies like
-        photography and working with air-dry clay, hanging out with{" "}
-        <a href="https://www.instagram.com/_sweetbabyhaze/" target="blank">
+        Outside of coding, I enjoy creative hobbies like photography, crafting
+        with air-dry clay, and spending time with{" "}
+        <a href="https://www.instagram.com/_sweetbabyhaze/" target="_blank">
           my rescue pup Hazel
         </a>
-        , or planning new adventures. I’m excited about the future of web
-        development and look forward to creating digital experiences that are as
-        inclusive as they are innovative.
+        . I’m driven to keep learning and building digital experiences that are
+        both innovative and accessible.
       </p>
     </Section>
   );
